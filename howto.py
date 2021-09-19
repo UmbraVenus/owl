3863ef6ddeab10f587a21a724249057744ef1a93
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-09-19 10:25:18 -0700

    added video tutorial

--- howto.py
+++ howto.py
@@ -4,7 +4,11 @@ img = Image.open("assets/img/edvisorly.png")
 
 def app():
     st.title("Video Tutorial")
-    st.info("Video will be here once parser questions are answered, Friday at 4 PM!")
+    #st.info("Video will be here once parser questions are answered, Friday at 4 PM!")
+    video_file = open('tutorial.mp4', 'rb')
+    video_bytes = video_file.read()
+
+    st.video(video_bytes)
     st.caption("All rights reserved @Edvisorly & Sage Ren & Kuhu Sharma 2021")
     st.image(img, width = 200)
     # screen cast can be easily done on streamlit
