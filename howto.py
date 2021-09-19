47aacd1e5dc019d42ee8769fa3592753781a2715
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-09-19 10:35:48 -0700

    fixed issue with video permission

--- howto.py
+++ howto.py
@@ -5,10 +5,8 @@ img = Image.open("assets/img/edvisorly.png")
 def app():
     st.title("Video Tutorial")
     #st.info("Video will be here once parser questions are answered, Friday at 4 PM!")
-    video_file = open('/tmp/tutorial.mp4', 'rb')
-    video_bytes = video_file.read()
 
-    st.video(video_bytes)
+    st.video('https://youtu.be/8_WD8JqJvL8')
     st.caption("All rights reserved @Edvisorly & Sage Ren & Kuhu Sharma 2021")
     st.image(img, width = 200)
     # screen cast can be easily done on streamlit
